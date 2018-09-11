e48b512e4978024f8698fcdd82e81eb1d8526026
Dextro Core <38873281+dextrocoin@users.noreply.github.com>
2018-09-11 12:44:20 -0300

    version 2.0.2.1
    version 2.0.2.1

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -13,7 +13,7 @@
  * for both dextrod and dextro-qt, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("Dextro Community");
+const std::string CLIENT_NAME("Dextro Core");
 
 /**
  * Client version number
